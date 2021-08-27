9c450846ff0902a7070d33457219860884eb7936
twaint <74901356+twaint@users.noreply.github.com>
2021-08-26 22:31:11 -0400

    module 5

---
+++ Module_5_Programming1/challenge1/challenge1.js
@@ -0,0 +1,21 @@
+let arr = [
+  "Westly Snipes",
+  "Nicholas Cage",
+  "Nasir Jones",
+  "Sean Carter",
+  "Sean Combs",
+  "Michael Jordan",
+  "Patrick Ewing",
+];
+
+firstName = [];
+lastName = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr.split([i])) {
+    firstName.push(arr[i]);
+  } else {
+    lastName.push(arr[i]);
+  }
+}
+ 
